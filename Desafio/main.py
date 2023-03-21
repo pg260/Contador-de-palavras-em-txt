ad71ce8b05b0d5d113afcf1007a8f657b86cab01
pg260 <spg2616@gmail.com>
2023-03-21 20:31:30 -0300

    cod reformulation

--- Desafio/main.py
+++ Desafio/main.py
@@ -1,20 +1,27 @@
 #Código feito por Pedro Gustavo
 
 arquivo = input("Digite o nome do arquivo para ser analizado (OBS: Sem o .txt no final): ").strip().lower()
-palavra = input("\nAgora digite a palavra que você deseja contar: ").strip().lower()
 print()
+
+cont = 0
 
 #tentando ler o arquivo
 try:
     with open(arquivo + ".txt".lower(), "r") as file:
         leitura = file.read().lower()
-        cont = leitura.count(palavra)
+        texto = leitura.split()
+
+        for palavra in texto:
+            for letras in palavra:
+                if(letras.isalpha()):
+                    cont += 1
+                    break
 
         #Checagem para saber se existe a palavra
         if(cont > 0):
-            print(f"A palavra '{palavra}' apareceu {cont} vezes.")
+            print(f"Apareceram {cont} palavras no texto.")
         else:
-            print(f"A palavra '{palavra}' não foi encontrada no texto.")
+            print(f"Nenhuma palavra foi encontrada no texto.")
 
 except FileNotFoundError:
     print("O arquivo não foi encontrado.")
